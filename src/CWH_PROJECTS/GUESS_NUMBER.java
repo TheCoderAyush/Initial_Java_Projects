288cd282b2a236e12655cc72403d4752c0555fd6
Ayush <thecoderayush@gmail.com>
2026-07-06 20:13:08 +0530

    Added History File  of User

--- src/CWH_PROJECTS/GUESS_NUMBER.java
+++ src/CWH_PROJECTS/GUESS_NUMBER.java
@@ -1,5 +1,8 @@
 package CWH_PROJECTS;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 class Game{
@@ -7,28 +10,29 @@ class Game{
    public  int userNumber;
    public  int noOfGuesses;
    public  int Guesses;
+   public String Name;
    //Constructor
     public Game(){
         Random rn = new Random();
          randomNumber= rn.nextInt(0,11);
     }
-    // Taking user Input
+
     public void takeUserInput(){
         Scanner sc= new Scanner(System.in);
             System.out.println("Give Your Number:- ");
             userNumber = sc.nextInt();
     }
-    // Setting no. of Guess
+
 
     public void setNoOfGuesses(){
 
             Guesses++;
         this.noOfGuesses=Guesses;
     }
-    //comparing
+
     public void isCorrectNumber(){
         if (userNumber==randomNumber){
-            System.out.println("Bravo..You Guessed it right!");
+            System.out.println("Bravo....You Guessed it right!");
             System.out.println();
             System.out.println("Good Job");
         }else{
@@ -40,25 +44,80 @@ class Game{
             }while (userNumber!=randomNumber);
         }
     }
-    //getting no. of Guesses
 
     public int getNoOfGuesses() {
+
         return noOfGuesses;
     }
-    // Nalla Adami
-    public void nalla(){
+
+    public void doItBetter(){
         if ( noOfGuesses>4){
-            System.out.println("MARGA BHAI AUR CODING CHHOD DE");
+            System.out.println("You can do Better.");
         }else{
             System.out.println("Finally.. You Guessed it!");
         }
+    }
+    public void  createFile(){
+        File data = new File("Initial_Java_Projects/src/CWH_PROJECTS/History.txt");
+        try{
+            data.createNewFile();
+            System.out.println("File created Successfully");
+        }catch(IOException e){
+            System.out.println(e);
+        }
+//
+    }
+    public void  storeData(){
+        try{
+            FileWriter fw= new FileWriter("Initial_Java_Projects/src/CWH_PROJECTS/History.txt",true);
+            fw.write("UserName : "+Name +"\n");
+            fw.write("Your Number was : "+randomNumber+"\n");
+            fw.write("Guesses : "+noOfGuesses + "\n");
+            fw.write("\n");
+            fw.close();
+
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+    }
+    public void userName(){
+        boolean condition = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name : ");
+        Name=sc.nextLine();
+        if (Name.matches("[a-zA-Z ]+")){
+
+            takeUserInput();
+//            Checking correctness
+            isCorrectNumber();
+
+//    counting no. of Guesses
+            setNoOfGuesses();
+
+            System.out.println("The Number of Guesses :"+getNoOfGuesses());
+
+            doItBetter();
+
+
+//        Storing Data
+          storeData();
+
+
+        }else{
+            System.out.println("Invalid name ! Give Valid Name ");
+            System.out.println("Program Ended");
+            condition = false;
+        }
+
+
     }
 }
 public class GUESS_NUMBER {
     public static void main(String[] args) {
         Game clue= new Game();
-        //Printing random number
-        System.out.println("Random Number is: "+clue.randomNumber);
+
 
         System.out.println("  --------------------Let The Game Begin--------------------");
         System.out.println();
@@ -67,22 +126,10 @@ public class GUESS_NUMBER {
         System.out.println("Choose Number Between 1 and 10");
         System.out.println();
 
-        clue.takeUserInput();
 
+        clue.userName();
 
-        clue.isCorrectNumber();
-
-        clue.setNoOfGuesses();
-
-        System.out.println();
-
-        System.out.println("The Number of Guesses :"+clue.getNoOfGuesses());
-
-        clue.nalla();
-
-
-
-
+//        clue.createFile();
     }
 
 }
